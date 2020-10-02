24ccab587ea580fb33879d9a859231ae6b86fcf2
yxk5255 <71482775+yxk5255@users.noreply.github.com>
2020-10-02 21:46:32 +0900

    Update lab5.py

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -5,6 +5,13 @@
 # Section: 1
 # Breakout: 1
 
+# Author: Yanling Wang yuw17@psu.edu
+# Collaborator:
+# Collaborator:
+# Collaborator:
+# Section: 1
+# Breakout: 1
+
 def is_palindrome1(s):
   """
   This function returns True if s is a palindrome, i.e.,
@@ -12,7 +19,19 @@ def is_palindrome1(s):
   You must implement this with a while loop that compares
   characters in s one by one going forward vs. going backward
   """
+
+s = input("Enter the string: ")
+k = s
+rev = ""
+while(len(k) > 0):
+  if(len(k) > 0):
+    a = k[-1]
+    k = k[:-1]
+    rev = rev + a
+if(rev == s):
   return True
+else:
+  return False
 
 def is_palindrome2(s):
   """
@@ -24,7 +43,12 @@ def is_palindrome2(s):
   Use negative index to get the last character and use
   slicing to get the middle of the string.
   """
-  return True
+s = input(("Enter a string:"))
+if(s==s[::-1]):
+      return True
+else:
+      print("Not a palindrome")
+      return False
 
 def is_palindrome3(s):
   """
@@ -36,6 +60,12 @@ def is_palindrome3(s):
   """
   return True
 
+s = input("Enter the string: ")
+if str(s) == str(s)[::-1]:
+  return True
+else:
+  return False
+
 def run():
   s = input("Enter a string: ")
   print(is_palindrome1(s))
@@ -44,3 +74,4 @@ def run():
 
 if __name__ == "__main__":
   run()
+
