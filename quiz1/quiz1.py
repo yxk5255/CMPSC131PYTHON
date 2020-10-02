47112df97b33b3baab4400b0e67dba00f8ae5e6c
yxk5255 <71482775+yxk5255@users.noreply.github.com>
2020-10-02 21:23:56 +0900

    Update quiz1.py

--- quiz1/quiz1.py
+++ quiz1/quiz1.py
@@ -1,23 +1,8 @@
-def f1(a):
-k = int(input("Enterryour number : "))
-if k<0 :
-    k = k*-1
-print(k) 
+def f1(n):
 
 def f2(n):
-sum = 0
-f = int(input("How many times should I add? -->"))
-for i in range(f):
-    x = int(input("What is your number> -->"))
-    if x<0:
-        x = x*-1
-    sum = sum + x
-print(sum)
-  
+
 def run():
-  n = int(input("What is your number? -->"))
-  print(f2(n))
-  return
 
 if True: 
   run()
